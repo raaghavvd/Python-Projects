65fd28d65bda394d0f246f965b38e99aa27bb102
raaghavvd <raaghavvd@gmail.com>
2017-09-25 18:00:51 +0530

    Add files via upload

---
+++ WebsiteDatabase/send_email.py
@@ -0,0 +1,19 @@
+from email.mime.text import MIMEText
+import smtplib
+def send_email(email,height,average_height,count):
+    from_email= #Enter your email
+    from_password= #Enter Password
+    to_email=email
+
+    subject="Height Data"
+    message="Hey there , your height is <strong>%s</strong> The average Height is <strong>%s</strong> from <strong>%s</strong>."% (height,average_height,count)
+    msg=MIMEText(message,'html')
+    msg['Subject']=subject
+    msg['To']=to_email
+    msg['From']=from_email
+
+    gmail=smtplib.SMTP('smtp.gmail.com',587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(from_email,from_password)
+    gmail.send_message(msg)
